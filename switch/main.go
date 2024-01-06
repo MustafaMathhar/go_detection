8e8720869e85d9655a74b53d0b2dfdb53de96341
Mustafa Mathhar <mathhar.business@gmail.com>
2024-01-06 19:05:12 +0300

    gpio

--- switch/main.go
+++ switch/main.go
@@ -2,38 +2,37 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"time"
+	"log"
 
-	rpio "github.com/stianeikeland/go-rpio/v4"
+	"periph.io/x/conn/v3/gpio"
+	"periph.io/x/conn/v3/gpio/gpioreg"
+	"periph.io/x/host/v3"
 )
 
+// Open Raspberry Pi GPIO
+
 func main() {
-	// Open Raspberry Pi GPIO
-	if err := rpio.Open(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	// Load all the drivers:
+	if _, err := host.Init(); err != nil {
+		log.Fatal(err)
 	}
-	defer rpio.Close()
 
-	// Use GPIO pin 17 (BCM numbering) as an input for a switch
-	pin := rpio.Pin(22)
-	pin.Input()
-	pin.PullUp()
-	pin.Detect(rpio.FallEdge)
-	// Loop to continuously read the state of the switch
+	// Lookup a pin by its number:
+	p := gpioreg.ByName("GPIO22")
+	if p == nil {
+		log.Fatal("Failed to find GPIO2")
+	}
+
+	fmt.Printf("%s: %s\n", p, p.Function())
+
+	// Set it as input, with an internal pull down resistor:
+	if err := p.In(gpio.PullDown, gpio.BothEdges); err != nil {
+		log.Fatal(err)
+	}
+
+	// Wait for edges as detected by the hardware, and print the value read:
 	for {
-		if pin.EdgeDetected() {
-			fmt.Println("Switch pressed")
-			pin.Detect(rpio.NoEdge)            // Disable further edge detection for now
-			time.Sleep(100 * time.Millisecond) // Sleep to debounce the switch
-			for pin.Read() == rpio.Low {
-				// Wait for the switch to be released
-				time.Sleep(100 * time.Millisecond)
-			}
-			pin.Detect(rpio.FallEdge) // Re-enable edge detection
-		}
-
-		time.Sleep(100 * time.Millisecond) // Add a short delay to avoid continuous polling
+		p.WaitForEdge(-1)
+		fmt.Printf("-> %s\n", p.Read())
 	}
 }
